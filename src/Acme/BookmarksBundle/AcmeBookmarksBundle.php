de465a0bd2fd71dac6c51bc0b9d1eb01df5355e6
sosantos <susana.o.santos@gmail.com>
2016-03-29 23:29:08 -0300

    criando bundle

---
+++ src/Acme/BookmarksBundle/AcmeBookmarksBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Acme\BookmarksBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AcmeBookmarksBundle extends Bundle
+{
+}
---
+++ src/Acme/BookmarksBundle/Entity/Page.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Acme\BookmarksBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Page
+ *
+ * @ORM\Table(name="page")
+ * @ORM\Entity(repositoryClass="Acme\BookmarksBundle\Repository\PageRepository")
+ */
+class Page
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="link", type="string", length=255)
+     */
+    private $link;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set link
+     *
+     * @param string $link
+     * @return Page
+     */
+    public function setLink($link)
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    /**
+     * Get link
+     *
+     * @return string 
+     */
+    public function getLink()
+    {
+        return $this->link;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Page
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
